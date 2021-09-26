32913d2386ae752dfb3821bef6b95606b7808d02
Blayts <Grehovnik-18@yandex.ru>
2021-09-26 22:57:19 +0400

    Add again console.log

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ console.log("Again hello");
 =======
 
 console.log("I say hello");
+<<<<<<< HEAD
 >>>>>>> f61fe35 (Add new console.log)
+=======
+
+console.log("I say hello");
+>>>>>>> 9503d94 (Add again console.log)
